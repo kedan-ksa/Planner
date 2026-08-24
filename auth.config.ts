c1fc8658eba7df0b92ae3683ef4676d4790bacfb
kedan-ksa <ahmad@kedan.com.sa>
2026-08-24 17:51:15 +0300

    fix: enable temporary Cloudflare public URL

--- auth.config.ts
+++ auth.config.ts
@@ -2,6 +2,7 @@ import type { NextAuthConfig } from "next-auth";
 import MicrosoftEntraID from "next-auth/providers/microsoft-entra-id";
 
 export default {
+  trustHost: true,
   providers: [
     MicrosoftEntraID({
       clientId: process.env.AZURE_AD_CLIENT_ID ?? "",
